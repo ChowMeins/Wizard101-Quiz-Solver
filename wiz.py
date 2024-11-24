d67ac872666c7d16e605721b95276fed3949580c
ChowMeins <jasonchau4627@gmail.com>
2024-11-24 03:39:27 -0600

    Headless chrome web driver added

--- wiz.py
+++ wiz.py
@@ -1,19 +1,31 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from dotenv import load_dotenv
 import json, os, time, sys, requests
 import whisper
+def sendKeysAtIntervals(element: WebElement, input: str, interval: float) -> None:
+    for char in input:
+        element.send_keys(char)
+        time.sleep(interval)
 
 def wizLogin(driver: WebDriver) -> None: # Locate buttons and enter text needed to login
-    login = driver.find_element(By.ID, 'loginUserName')
-    password = driver.find_element(By.ID, 'loginPassword')
-    loginButton = driver.find_element(By.CLASS_NAME, 'wizardButtonInput')
-    login.send_keys(os.getenv("WIZ_USERNAME"))
-    password.send_keys(os.getenv("WIZ_PASSWORD"))
-    loginButton.click()
+    wait = WebDriverWait(driver, 300)
+    try:
+        login = wait.until(EC.presence_of_element_located((By.ID, 'loginUserName')))
+        password = wait.until(EC.presence_of_element_located((By.ID, 'loginPassword')))
+        loginButton = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'wizardButtonInput')))
+        sendKeysAtIntervals(login, os.getenv("WIZ_USERNAME"), 0.025)
+        sendKeysAtIntervals(password, os.getenv("WIZ_PASSWORD"), 0.025)
+        time.sleep(0.5)
+        loginButton.click()
+    except Exception as e:
+        print('Exception:', e)
+        driver.get_screenshot_as_file("login_error.png")
 
 def loadTrivia(driver: WebDriver, questions: dict) -> None:
     with open("quizzes.json", "r") as file:
@@ -40,48 +52,52 @@ def navigateTrivia(driver: WebDriver, questions: dict, numSolved: int) -> None:
                 numSolved += 1
                 continue
         except Exception as e:
-            print(f"Exception type: {type(e)}")
-            print(f"Exception message: {e}")
+            print('No come back tomorrow text found. Proceed to next quiz')
             print(f'Number of Quizzes solved: {numSolved}')
             solveQuestions(driver, questions, key, numSolved)
             numSolved += 1
 
 def solveQuestions(driver: WebDriver, questions: dict, triviaName: str, numSolved: int) -> None:
     questionsSolved = 0
+    wait = WebDriverWait(driver, 10)
     while(questionsSolved < 12):
-        wait = WebDriverWait(driver, 300)
-        nextButton = wait.until(EC.element_to_be_clickable((By.ID, 'nextQuestion')))
-        quizQuestion = driver.find_element(By.CLASS_NAME, 'quizQuestion')
-        answers = driver.find_elements(By.CSS_SELECTOR, '.answer.fadeIn')
-        qa_pairs = questions.get(triviaName)
-        possible_answers = []
-        correctAnswerFound = False
-        # If correct answer choice matches, append answer text to possible_answers (multiple question may have same answer)
+        try:
+            nextButton = wait.until(EC.element_to_be_clickable((By.ID, 'nextQuestion')))
+            quizQuestion = driver.find_element(By.CLASS_NAME, 'quizQuestion')
+            answers = driver.find_elements(By.CSS_SELECTOR, '.answer.fadeIn')
+            qa_pairs = questions.get(triviaName)
+            possible_answers = []
+            correctAnswerFound = False
+            # If correct answer choice matches, append answer text to possible_answers (multiple question may have same answer)
 
-        if qa_pairs == None:
-            print('No QA Pairs Found')
-        for pair in qa_pairs:
-            if quizQuestion.text == pair[0]:
-                possible_answers.append(pair[1])
-        answerChoices = []
-        for answer in possible_answers:
-                for answerDiv in answers:
-                    answerText = answerDiv.find_element(By.CLASS_NAME, 'answerText')
-                    answerChoices.append(answerText.text)
-                    answerBox = answerDiv.find_element(By.CLASS_NAME, 'largecheckbox')
-                    if(answerText.text in possible_answers):
-                        answerBox.click()
-                        nextButton.click()
-                        questionsSolved += 1
-                        correctAnswerFound = True
+            if qa_pairs == None:
+                print('No QA Pairs Found')
+            for pair in qa_pairs:
+                if quizQuestion.text == pair[0]:
+                    possible_answers.append(pair[1])
+            answerChoices = []
+            for _ in possible_answers:
+                    for answerDiv in answers:
+                        answerText = answerDiv.find_element(By.CLASS_NAME, 'answerText')
+                        answerChoices.append(answerText.text)
+                        answerBox = answerDiv.find_element(By.CLASS_NAME, 'largecheckbox')
+                        if(answerText.text in possible_answers):
+                            answerBox.click()
+                            time.sleep(1)
+                            nextButton.click()
+                            questionsSolved += 1
+                            correctAnswerFound = True
+                            break
+                    if (correctAnswerFound):
                         break
-                if (correctAnswerFound):
-                    break
-        if (correctAnswerFound == False):
-            print(quizQuestion.text)
-            for a in answerChoices:
-                print(a)
-        print(f'Question #{questionsSolved} solved!')
+            if (correctAnswerFound == False):
+                print(quizQuestion.text)
+                for a in answerChoices:
+                    print(a)
+            print(f'Question #{questionsSolved} solved!')
+        except:
+            driver.get_screenshot_as_file('question_error.png')
+            break
 
         
     claimRewardFirstButton = wait.until(EC.element_to_be_clickable((By.LINK_TEXT, "CLAIM YOUR REWARD")))
@@ -89,11 +105,11 @@ def solveQuestions(driver: WebDriver, questions: dict, triviaName: str, numSolve
     driver.switch_to.frame('jPopFrame_content')
     claimRewardFinalButton = wait.until(EC.element_to_be_clickable((By.ID, "submit")))
     claimRewardFinalButton.click()
-    time.sleep(3)
+    time.sleep(1)
     try:
         recaptchaWait = WebDriverWait(driver, 5)
         frames = driver.find_elements(By.TAG_NAME, 'iframe')
-        print(len(frames))
+        #print(len(frames))
         for i in range(len(frames)):
             titleName = frames[i].get_attribute('title')
             if titleName == 'recaptcha challenge expires in two minutes':
@@ -105,19 +121,18 @@ def solveQuestions(driver: WebDriver, questions: dict, triviaName: str, numSolve
         audioUrl = audioElement.get_attribute('src')
         downloadMP3(audioUrl)
         audioTranscribed = transcribeAudio('audio_captcha.mp3')
-        print('Translated audio:', audioTranscribed)
+        #print('Translated audio:', audioTranscribed)
         audioInput = recaptchaWait.until(EC.presence_of_element_located((By.ID, 'audio-response')))
-        print(audioInput.get_attribute('tag'))
-        audioInput.send_keys(audioTranscribed)
+        sendKeysAtIntervals(audioInput, audioTranscribed, 0.025)
         verifyButton = recaptchaWait.until(EC.element_to_be_clickable((By.ID, 'recaptcha-verify-button')))
         verifyButton.click()
     except Exception as e:
         print(e)
         print('reCAPTCHA not found.')
+
     driver.switch_to.default_content()
 
     wait.until(EC.visibility_of_element_located((By.LINK_TEXT, 'TAKE ANOTHER QUIZ!')))
-    time.sleep(1)
 
 def downloadMP3(url: str):
     response = requests.get(url)
@@ -134,8 +149,12 @@ def transcribeAudio(file: str) -> str:
 def main():
     load_dotenv(override=True) # Load env variables from .env file
     questions_map = {}
-
-    driver = webdriver.Chrome()
+    
+    chromeOptions = Options()
+    chromeOptions.add_argument("--headless")
+    chromeOptions.add_argument("--disable-gpu")
+    chromeOptions.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36")
+    driver = webdriver.Chrome(options=chromeOptions)
     driver.get("https://www.wizard101.com/game/trivia")
     wizLogin(driver)
     loadTrivia(driver, questions_map)
